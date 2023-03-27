ee03843367637459642f0c50d5619d0f11059bf8
Nurzhan <mrb.nurzhan@gmail.com>
2023-03-27 17:15:59 +0600

    changing color of tiles in js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 import { Grid } from "./grid.js";
+import { Tile } from "./tile.js";
 const gameboard = document.getElementById("game-board");
 
 const grid = new Grid(gameboard);
-grid.getRandomEmptyCell().linkTile(new tile(gameboard));
+grid.getRandomEmptyCell().linkTile(new Tile(gameboard));
+grid.getRandomEmptyCell().linkTile(new Tile(gameboard));
